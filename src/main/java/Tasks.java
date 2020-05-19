bce83fc8558cb0a71ff79506a6823774ebc46f61
vtsyryuk <vtsyryuk@gmail.com>
2020-05-20 03:22:58 +0800

    implemented UnionFind and solved maxCircle task

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -1,7 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public final class Tasks {
+    static final int[] EMPTY_INT_ARRAY = new int[0];
+
     private Tasks() {
+    }
+
+    // Complete the maxCircle function below.
+    static int[] maxCircle(int[][] queries) {
+        if (null == queries) {
+            return EMPTY_INT_ARRAY;
+        }
+        final UnionFind uf = new UnionFind();
+        final int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            uf.union(queries[i][0], queries[i][1]);
+            res[i] = uf.max;
+        }
+        return res;
+    }
+
+    static class UnionFind {
+        final Map<Integer, Integer> parents = new HashMap<>();
+        final Map<Integer, Integer> sizes = new HashMap<>();
+        int max = 0;
+
+        public UnionFind() {
+        }
+
+        public void union(int v1, int v2) {
+            if (!parents.containsKey(v1)) {
+                parents.put(v1, v1);
+                sizes.put(v1, 1);
+            }
+
+            if (!parents.containsKey(v2)) {
+                parents.put(v2, v2);
+                sizes.put(v2, 1);
+            }
+
+            final int p1 = find(v1), p2 = find(v2);
+            if (p1 == p2) return;
+
+            final int s1 = sizes.get(p1), s2 = sizes.get(p2);
+            if (s1 < s2) {
+                parents.put(p1, p2);
+                sizes.put(p2, s1 + s2);
+            } else {
+                parents.put(p2, p1);
+                sizes.put(p1, s1 + s2);
+            }
+            if (s1 + s2 > max) max = s1 + s2;
+        }
+
+        public int find(int v) {
+            while (parents.get(v) != v) {
+                parents.put(v, parents.get(parents.get(v)));
+                v = parents.get(v);
+            }
+            return v;
+        }
     }
 
     // Complete the repeatedString function below.
--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TasksTest {
@@ -30,7 +31,6 @@ public class TasksTest {
         assertEquals(8, Tasks.superDigit("9a8   7d5sawdw", 4));
     }
 
-
     @Test
     void testPalindrome() {
         assertEquals(0, Tasks.palindrome(null));
@@ -41,5 +41,41 @@ public class TasksTest {
         assertEquals(1, Tasks.palindrome("Z"));
         assertEquals(7, Tasks.palindrome("mokkori"));
         assertEquals(5, Tasks.palindrome("aabaa"));
+    }
+
+    @Test
+    void testLargestFriendCircle() {
+        assertArrayEquals(Tasks.EMPTY_INT_ARRAY, Tasks.maxCircle(null));
+        assertArrayEquals(Tasks.EMPTY_INT_ARRAY, Tasks.maxCircle(new int[0][]));
+        assertArrayEquals(Tasks.EMPTY_INT_ARRAY, Tasks.maxCircle(new int[0][0]));
+
+        assertArrayEquals(new int[]{2}, Tasks.maxCircle(new int[][]{{1, 2}}));
+
+        final int[][] input1 = new int[][]{
+                {1, 2},
+                {1, 3}
+        };
+        final int[] expected1 = new int[]{2, 3};
+        assertArrayEquals(expected1, Tasks.maxCircle(input1));
+
+        final int[][] input2 = new int[][]{
+                {1000000000, 23},
+                {11, 3778},
+                {7, 47},
+                {11, 1000000000}
+        };
+        final int[] expected2 = new int[]{2, 2, 2, 4};
+        assertArrayEquals(expected2, Tasks.maxCircle(input2));
+
+        final int[][] input3 = new int[][]{
+                {1, 2},
+                {3, 4},
+                {1, 3},
+                {5, 7},
+                {5, 6},
+                {7, 4}
+        };
+        final int[] expected3 = new int[]{2, 2, 4, 4, 4, 7};
+        assertArrayEquals(expected3, Tasks.maxCircle(input3));
     }
 }
